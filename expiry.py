6fc31842c0c64459e36a8827cd67d8708fd1bba9
Peter Garbett <petegarbett1958@gmail.com>
2024-07-15 01:55:28 +0100

    Cope with extended filesnames

--- expiry.py
+++ expiry.py
@@ -9,17 +9,17 @@
 #   by date.
 #
 
-import sys
 import os
+import shutil
 import glob
 import datetime
-from PIL import Image 
-import tidy
 
 dryRun = False
 
 
 def txtTimestampToTime(filename):
+
+    debug = False
 
     # Convert something of the form
     # 2024_03_06_14_24_21_423461_l<junk>.<junk>
@@ -41,14 +41,34 @@ def txtTimestampToTime(filename):
         MicrosecondsTXT = timestamp.split(".")[1]
         MicrosecondsTXT = MicrosecondsTXT.split("_")[0]
         MicrosecondsTXT = MicrosecondsTXT.strip()
+        if debug:
+            print(filename, "microseconds:", MicrosecondsTXT)
+    except Exception as e:
+        print(filename, "Error extracting timestamp ", e)
+        when = datetime.datetime.now()  # default to new
+        return (filename, when)
+
+    try:
+        if debug:
+            print("convert:", MicrosecondsTXT)
         Microseconds = int(MicrosecondsTXT)
+        if debug:
+            print("converted:", Microseconds)
+
+        # Remove the extra junk digits we have in some cases
+
+        stampNoMs = stampNoMs.split(".")[0]
         when = datetime.datetime.strptime(stampNoMs, "%Y-%m-%d %H:%M:%S")
         when = when.replace(microsecond=Microseconds)
     except Exception as e:
-        print("Error extracting timestamp ", e)
+        print(filename, "Error extracting microsecond timestamp ", e)
         when = datetime.datetime.now()  # default to new
 
     return (filename, when)
+
+
+from os import listdir
+from PIL import Image
 
 
 def check_image(base, filename):
@@ -63,13 +83,14 @@ def check_image(base, filename):
         I.close()
         return True
     except (IOError, SyntaxError) as e:
-        if debug:
+        if True:
             print("Bad file:", filename, e)  # print out the names of corrupt files
         return False
 
 
 def delete_old_image_files(folder, ageLimit):
 
+    debug = True
 
     # folder is the name of the folder in which we
     # have to perform the delete operation
@@ -93,9 +114,8 @@ def delete_old_image_files(folder, ageLimit):
                 command = "sudo rm -f " + folder + imgnames[index] + " > /dev/null 2>&1"
                 if not dryRun:
                     print("Remove corrupt jpg via command :", command)
-                    os.system(command)
-            except Exception as e:
-                print(e)
+                    response = os.system(command)
+            except:
                 pass
 
     imgnames = sorted(glob.glob("2*.jpg"))
@@ -108,16 +128,15 @@ def delete_old_image_files(folder, ageLimit):
     for index in range(len(when)):
         if when[index][1] < expiryTime:
             if not dryRun:
-                os.system(
+                response = os.system(
                     "sudo rm -f " + when[index][0] + " > /dev/null 2>&1"
                 )
             print("Delete:", when[index][0])
         else:
             break  # List is sorted by date so there won't be any more
 
-    # Remove local/non_local duplicates
 
-    tidy.tidy(folder)
+import sys
 
 if __name__ == "__main__":
 
@@ -128,7 +147,7 @@ if __name__ == "__main__":
         print(
             "Incorrect usage! Should be : <path>/expiry <directory> <time limit in days>"
         )
-        sys.exit()
+        exit()
 
     try:
         ageLimit = int(inputargs[1])
@@ -136,7 +155,7 @@ if __name__ == "__main__":
         print(
             "Incorrect usage! Should be : <path>/expiry <directory> <time limit in days>"
         )
-        sys.exit()
+        exit()
 
     print(
         "Delete corrupt images and images older than ",
@@ -146,5 +165,3 @@ if __name__ == "__main__":
     )
 
     delete_old_image_files(inputargs[0], ageLimit)
-
-    sys.exit()
